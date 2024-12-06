214959477bf32f39f49733a815d1af930fe1c4f2
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-12-06 16:18:48 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import Hero from "./components/Hero"
-
+import { Outlet } from "react-router-dom"
+import { FloatingNav } from "./components/ui/floating-navbar"
+import OpenIconSpeedDial from "./components/ui/SpeedDial"
+import { useUserContext } from "./context/UserContext";
+import { useEffect } from "react";
+import { useUser } from "@clerk/clerk-react";
+import axios from "axios";
 function App() {
+  const BaseURL = import.meta.env.VITE_BASE_URL;
+  const { isLoaded, isSignedIn, user } = useUser();
+  const { setUserData } = useUserContext();
+
+  const navbarItems = [
+    { name: 'Home', link: 'home' }, { name: 'social', link: 'social' }, { name: 'News', link: 'News' }, { name: 'Managment', link: 'Managment' }
+  ]
+  useEffect(() => {
+    if (!user && !isSignedIn) {
+      setUserData(null)
+      return
+    } else {
+
+      const bodyData = {
+        name: user.fullName,
+        email: user.primaryEmailAddress?.emailAddress,
+        _id: user.id,
+        userPFP: user.imageUrl,
+      }
+      console.log(bodyData);
+
+      axios
+        .post(`${BaseURL}signUp`, bodyData)
+        .then((response) => {
+          console.log("Success:", response.data.userData);
+          setUserData(response.data.userData)
+        }).catch((error) => {
+          console.log(error);
+        })
+    }
+  }, [isLoaded, isSignedIn, user])
 
   return (
-    <div>
-      <Hero />
+    <div className="grow flex flex-col">
+      <FloatingNav navItems={navbarItems} />
+      <Outlet />
+      <OpenIconSpeedDial />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,55 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './styles/fonts.css'
+import './styles/Post.css'
+import './styles/ExtraComponents.css'
+import './styles/navbar.css'
+import { ToastContainer } from 'react-toastify';
+import "yet-another-react-lightbox/styles.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './components/HomePage.tsx'
+import { ClerkProvider, SignIn, SignUp } from '@clerk/clerk-react'
+import CenteredPage from './components/CenteredPage.tsx'
+import NotFoundPage from './components/NotFoundPage.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import SocialPage from './components/Pages/SocialPage.tsx'
+import 'react-toastify/dist/ReactToastify.css';
+import Managment from './components/Pages/Managment.tsx'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const queryClient = new QueryClient()
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/home", element: <HomePage /> },
+      { path: "/continental", element: <HomePage /> },
+      { path: "/managment", element: <CenteredPage><Managment /></CenteredPage> },
+      { path: "/Social", element: <SocialPage /> },
+      { path: "/sign-in", element: <CenteredPage className="mt-20"><SignUp routing='hash' forceRedirectUrl={'/continental'} /></CenteredPage> },
+      { path: "/sign-up", element: <CenteredPage className="mt-20"><SignIn routing='hash' forceRedirectUrl={'/continental'} /></CenteredPage> },
+      { path: "*", element: <CenteredPage><NotFoundPage /></CenteredPage> },
+    ]
+  }
+])
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* <App /> */}
+      
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </ClerkProvider>
+  // </StrictMode>
 )
