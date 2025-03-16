7049ae13395d41ab430678b98bc97fa4456cad6a
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2025-03-16 21:00:17 +0200

    update policy css

--- src/components/Pages/PrivacyPolicyPage.tsx
+++ src/components/Pages/PrivacyPolicyPage.tsx
@@ -11,7 +11,7 @@ export default function PrivacyPolicyPage() {
         }
       }, []);
     return (
-        <div className="text-neutral-600 dark:text-neutral-200 p-20">
+        <div className="text-neutral-600 dark:text-neutral-200 py-20 px-5 md:px-20">
             <h1>Privacy Policy</h1>
             <p>Last updated: March 16, 2025</p>
             <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
